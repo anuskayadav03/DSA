9d37587eb129b46c6432ce52f649f3f829eba9f3
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-07-18 14:42:27 +0530

    java basics

--- javafolder/src/fuctions.java
+++ javafolder/src/fuctions.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class fuctions {
-    public static void MyName(String name){
+   /* public static void MyName(String name){
         System.out.print("your name is : " +name);
         return;
     }
@@ -9,5 +9,24 @@ public class fuctions {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         MyName(name);
+        }
+       }
+       */
+
+    //SUM OF TWO NUMBERS
+public static int CalculateSum(int a , int b){
+    int sum = (a+b);
+    return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+       int sum = CalculateSum(a,b);
+        System.out.println("The sum is: "+sum);
     }
 }
+
+
+//MULTIPLY 2 NUMBER AND RETURN PRODUCT
+
