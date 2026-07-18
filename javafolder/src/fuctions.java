bbd567e870b94ff31bdd9dd7c8b2eac9ca82a15e
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-07-18 15:16:14 +0530

    java basics

--- javafolder/src/fuctions.java
+++ javafolder/src/fuctions.java
@@ -14,7 +14,7 @@ public class fuctions {
        */
 
     //SUM OF TWO NUMBERS
-public static int CalculateSum(int a , int b){
+/*public static int CalculateSum(int a , int b){
     int sum = (a+b);
     return sum;
     }
@@ -26,7 +26,39 @@ public static int CalculateSum(int a , int b){
         System.out.println("The sum is: "+sum);
     }
 }
-
+*/
 
 //MULTIPLY 2 NUMBER AND RETURN PRODUCT
+/*public static int product(int a,int b){
+	int c=a*b;
+	return c;
+}
+public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=product(a,b);
+    System.out.println("the product is : "+c);
+}
+}
+ */
 
+//FIND FACTORIAL OF A NUMBER
+public static void FindFactorial(int num){
+    int factorial = 1;
+    if (num<0){
+        System.out.println("invalid number");
+        return;
+    }
+    for (int i =num; i>=1; i--) {
+        factorial = factorial * i;
+    }
+    System.out.println(factorial);
+    return;
+}
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    FindFactorial(num);
+ }
+}
