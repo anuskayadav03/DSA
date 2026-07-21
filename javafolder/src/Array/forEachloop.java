2bf82dc708d6cc65060c342fec12200d589f7190
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-07-21 23:19:14 +0530

    foreach loop done

---
+++ javafolder/src/Array/forEachloop.java
@@ -0,0 +1,8 @@
+public class forEachloop {
+    public static void main(String[] args) {
+       int[] num={5,8,2,6,9};
+       for(int element:num){
+           System.out.print(element +" ");
+       }
+    }
+}
